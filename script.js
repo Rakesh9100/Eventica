0ab4f3fef8d922ae78099ac198c5adbe98c19609
Rakesh Roshan <roshanrakesh7362@gmail.com>
2025-01-14 23:09:59 +0530

    Fixed minor logic

--- script.js
+++ script.js
@@ -320,15 +320,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const seeMoreBtn = document.getElementById("see-more-btn");
     const extraTestimonials = document.querySelector(".extra-testimonials");
 
-    seeMoreBtn.addEventListener("click", () => {
-        // Toggle the "hidden" class
-        extraTestimonials.classList.toggle("hidden");
+    if (seeMoreBtn && extraTestimonials) {
+        seeMoreBtn.addEventListener("click", () => {
+            // Toggle the "hidden" class
+            extraTestimonials.classList.toggle("hidden");
 
-        // Update button text and icon
-        if (extraTestimonials.classList.contains("hidden")) {
-            seeMoreBtn.innerHTML = 'See More <i class="fas fa-chevron-down"></i>';
-        } else {
-            seeMoreBtn.innerHTML = 'See Less <i class="fas fa-chevron-up"></i>';
-        }
-    });
+            // Update button text and icon
+            if (extraTestimonials.classList.contains("hidden")) {
+                seeMoreBtn.innerHTML = 'See More <i class="fas fa-chevron-down"></i>';
+            } else {
+                seeMoreBtn.innerHTML = 'See Less <i class="fas fa-chevron-up"></i>';
+            }
+        });
+    }
 });
